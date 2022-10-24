e5ef5994eb2d78d5caadfec99b947ec9c4cd701e
jncfa <20467009+jncfa@users.noreply.github.com>
2022-10-24 14:48:54 +0000

    24-10 update

--- tests/utils.cc
+++ tests/utils.cc
@@ -1,17 +1,9 @@
 /**
- * @file serialization_test.cc
+ * @file utils.cc
  * @brief Testing the serialization of multiple components of the TRUSTID image
- * processing library.
+ * processing library.113
  * @author jncfa
- * @date 2019-10-30
- */
-
-/**
- * @brief
- *
- * @param argc
- * @param argv
- * @return int
+ * @date 2022-10-22
  */
 
 #include <dlib/serialize.h>
@@ -27,15 +19,12 @@ namespace image = trustid::image;
 namespace impl = trustid::image::impl;
 
 int main(int argc, char const *argv[]) {
-  cv::Mat3b random_image(100, 100);
-  cv::randu(random_image, cv::Scalar(0, 0, 0), cv::Scalar(256, 256, 256));
-
   // open image with opencv, serialize it with dlib, deserialize it and display
   // both side by side
-  cv::Mat image = cv::imread(
-      "C:\\Users\\jncfa\\source\\repos\\trustid_image_processing_"
-      "win\\tests\\test_resources\\images\\000288_00925786.jpg");
+  cv::Mat image(100, 100, cv::CV_64FC1);
   cv::Mat image2;
+
+  cv::randu(image, cv::Scalar(0, 0, 0), cv::Scalar(256, 256, 256));
 
   // serialize image and deserialize it into a new image
   std::stringstream ss;
